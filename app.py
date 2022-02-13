3a71e5336b4039935b393847a5b9a30b719a015f
Joel-Philip-Adventures <99598909+Joel-Philip-Adventures@users.noreply.github.com>
2022-02-13 09:16:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,12 @@ def time_post():
               
               
             return render_template('time.html', result=answer)
+	
+#Creating my own Page
+@app.route('/Dubai')
+def python_apps_page():
+	return render_template('Dubai.html')
+
 
          
 
